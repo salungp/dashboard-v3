75fcc21ff2e955595560eade1c65a6867fe6feef
salungprastyo <salungprastyo@gmail.com>
2021-05-27 01:38:58 +0800

    Fix name js code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,18 @@
 // Activated tooltip function from bootstrap
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+var TooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var TooltipList = TooltipTriggerList.map(function (TooltipTriggerEl) {
+  return new bootstrap.Tooltip(TooltipTriggerEl)
 })
 
 // Define all component to make sidebar effect
-var buttonToggle = document.querySelector('.button-toggle');
-var contentClass = document.querySelector('.content');
-var sidebarClass = document.querySelector('.sidebar');
-var contentHeaderClass = document.querySelector('.content-header');
+var ButtonToggle = document.querySelector('.button-toggle');
+var ContentClass = document.querySelector('.content');
+var SidebarClass = document.querySelector('.sidebar');
+var ContentHeaderClass = document.querySelector('.content-header');
 
 // Event to make sidebar effect
-buttonToggle.addEventListener('click', function() {
-	contentClass.classList.toggle('content-active');
-	sidebarClass.classList.toggle('sidebar-active');
-	contentHeaderClass.classList.toggle('content-header-active');
+ButtonToggle.addEventListener('click', function() {
+	ContentClass.classList.toggle('content-active');
+	SidebarClass.classList.toggle('sidebar-active');
+	ContentHeaderClass.classList.toggle('content-header-active');
 })
