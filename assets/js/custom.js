9285d2f971438e4b87b3caa0d285b2b553809dd2
salungprastyo <salungprastyo@gmail.com>
2021-05-27 01:16:51 +0800

    Remove useless bootstrap files, add name and id to input, add some functional comment to css and js code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,16 @@
+// Activated tooltip function from bootstrap
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+// Define all component to make sidebar effect
 var buttonToggle = document.querySelector('.button-toggle');
 var contentClass = document.querySelector('.content');
 var sidebarClass = document.querySelector('.sidebar');
 var contentHeaderClass = document.querySelector('.content-header');
 
+// Event to make sidebar effect
 buttonToggle.addEventListener('click', function() {
 	contentClass.classList.toggle('content-active');
 	sidebarClass.classList.toggle('sidebar-active');
